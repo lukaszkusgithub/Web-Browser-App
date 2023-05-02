01d2bdde41d75ccc819c2cf5490f06417a481584
Luryxyt <36661311+lukaszkusgithub@users.noreply.github.com>
2023-05-02 15:52:54 +0200

    Add laucher icon

--- app/src/main/java/com/example/web_browser/MainActivity.kt
+++ app/src/main/java/com/example/web_browser/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.web_browser
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.web_browser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
